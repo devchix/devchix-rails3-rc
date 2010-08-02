f44109e0d431e1546e0723cb80d022ae769724a3
Nola Stowe <nolastowe@nola.gateway.2wire.net>
2010-08-01 19:57:26 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevchixRails2Rc::Application.config.secret_token = '08962d48ce882d345793f7893962d07d259b2ce078f7a9c5258b86408afc49e9732a365acd06c0152a7f4cf6449dbbd1ced9bd1a41b6f315a4471fe294843651'

0065bbda54eab817d2f04a3b7a500f37755c63a0
Nola Stowe <nola@rubygeek.com>
2010-11-13 13:54:37 -0600

    fixed merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,26 +8,13 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
   has_one :profile
-<<<<<<< HEAD
   after_create :build_profile
-=======
-  after_create :create_user_profile
->>>>>>> 9b5f9e90e7094fa8e2bf067b8377c462dbe49dc8
-
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :lockable, :timeoutable and :activatable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
-<<<<<<< HEAD
-  
-=======
 
-
+   # so users can login by email or username, not tested yet
    # to look up a user by email or username
->>>>>>> 9b5f9e90e7094fa8e2bf067b8377c462dbe49dc8
    def self.find_for_authentication(conditions={})
     if conditions[:login] =~ /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i # email regex
       conditions[:email] = conditions[:login]
@@ -35,19 +22,11 @@ class User < ActiveRecord::Base
     end
     super
   end
-<<<<<<< HEAD
   
+  private
   def build_profile
     # tried using self.build_profile but didn't work. Meh. moving on
     self.profile = Profile.new
     self.profile.save(:validate => false) 
-=======
-
-  private
-
-  def create_user_profile
-    self.profile = Profile.new
-    self.profile.save(false)  #save without validating
->>>>>>> 9b5f9e90e7094fa8e2bf067b8377c462dbe49dc8
   end
 end
